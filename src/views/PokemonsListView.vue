0431aeb7705a953d6137898e025ecb1e63a88929
oscar barreto <barretooscar1408@gmail.com>
2024-06-06 18:08:02 -0500

    Listado de 10 pokemones

--- src/views/PokemonsListView.vue
+++ src/views/PokemonsListView.vue
@@ -7,8 +7,18 @@
     </div>
     <div class="row p-3 mt-3 mb-3 d-flex justify-content-center">
       <div v-for="pokemon in Pokemons" :key="pokemon.name" class="col-sm-10 mb-3">
-            <img :src="pokemon.image" alt="">
+        <div @click="watchDetail(pokemon.name)" class="card d-flex justify-content-center align-items-center">
+          <div class="card-title">
+            <img :src="pokemon.image" alt="">  <!--Imagen y nombre-->
             <h4>{{ pokemon.name }}</h4>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-auto">
+        <button @click="previousPage" :disabled="page === 1" class="btn btn-primary mr-2">Anterior</button>
+        <button @click="nextPage" class="btn btn-primary">Siguiente</button>
       </div>
     </div>
   </div>
@@ -23,6 +33,9 @@ export default {
   data() {
     return {
       Pokemons: [],
+      page: 1,
+      pageSize: 10,
+      totalPokemons: 90 
     };
   },
   async created() {
@@ -30,10 +43,30 @@ export default {
   },
   methods: {
     async fetchPokemons() {
-      const startIndex = (this.page - 1) * this.pageSize + 1;
+      const startIndex = (this.page - 1) * this.pageSize + 1; //inicio y fin paginacion
       const endIndex = startIndex + this.pageSize - 1;
       this.Pokemons = await PokemonServices.getPokemons(startIndex, endIndex > this.totalPokemons ? this.totalPokemons : endIndex);
     },
+    async watchDetail(name) {
+      this.$router.push({
+        name: 'PokemonDetail',
+        params: {
+          name: name
+        }
+      });
+    },
+    async nextPage() {  //incrementa la pag y actualiza la lista
+      if ((this.page * this.pageSize) < this.totalPokemons) { 
+        this.page++;
+        await this.fetchPokemons();
+      }
+    },
+    async previousPage() {  //decrementa
+      if (this.page > 1) {
+        this.page--;
+        await this.fetchPokemons();
+      }
+    }
   }
 };
 </script>
